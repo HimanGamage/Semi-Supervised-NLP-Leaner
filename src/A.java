8dc880923bb488521078680f6efa1c9bff703250
Himan Gamage <mail@himangamage.info>
2015-09-05 22:38:15 +0530

    Wordnet controller

--- src/A.java
+++ src/A.java
@@ -3,6 +3,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.Iterator;
+import java.util.Set;
+
+import net.sf.extjwnl.JWNLException;
+import net.sf.extjwnl.data.IndexWord;
+import net.sf.extjwnl.data.POS;
+import net.sf.extjwnl.dictionary.Dictionary;
 
 import com.hp.hpl.jena.ontology.DatatypeProperty;
 import com.hp.hpl.jena.ontology.ObjectProperty;
@@ -17,32 +23,32 @@ import com.hp.hpl.jena.vocabulary.*;
 
 public class A {
 
-	public static void main(String args[]){
-		JenaHandler handler=JenaHandler.getInstance();
-		/*
-		 * This is a test class for run the program
-		 * 
-		 * 
-		 * 
-		 */
+
+	public static void main(String args[]) throws JWNLException{
 		
+		//wordnet test code 
+		Dictionary dictionary = Dictionary.getDefaultResourceInstance();
+		 IndexWord word=dictionary.getIndexWord(POS.VERB, "talk");
 		
+		Set<String> set2=new WordnetController().listOperation(word);
+    	for(String a:set2){
+    		System.out.print(a+", ");
+    	}
+		//--
 		
-		
-		
-	
+    	JenaHandler handler=JenaHandler.getInstance();
 			
-		try {
-			//handler.createDataTypeProperty("hasScore", "Player", "Player scores runs",XSD.xstring);
-		//	handler.addIndividuals("Batsman","Dilshan");
-		//	handler.addIndividuals("Team","Sri Lanka");
-			//	handler.addIndividuals("Team","India");
-			//handler.addObjectTypeProperty("India","playedAgainest","Sri Lanka");
-			handler.addDataTypeProperty("Sangakkara", "hasScore", "100");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			//handler.createDataTypeProperty("hasScore", "Player", "Player scores runs",XSD.xstring);
+//		//	handler.addIndividuals("Batsman","Dilshan");
+//		//	handler.addIndividuals("Team","Sri Lanka");
+//			//	handler.addIndividuals("Team","India");
+//			//handler.addObjectTypeProperty("India","playedAgainest","Sri Lanka");
+//			handler.addDataTypeProperty("Sangakkara", "hasScore", "100");
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 			
 		
 		
