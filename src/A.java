78602cfa21cb55b6d08195cf23e8be645730e8e2
Himan Gamage <mail@himangamage.info>
2015-09-05 10:12:38 +0530

    Default project

---
+++ src/A.java
@@ -0,0 +1,50 @@
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Iterator;
+
+import com.hp.hpl.jena.ontology.DatatypeProperty;
+import com.hp.hpl.jena.ontology.ObjectProperty;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.ontology.Profile;
+import com.hp.hpl.jena.ontology.ProfileRegistry;
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.vocabulary.*;
+
+public class A {
+
+	public static void main(String args[]){
+		JenaHandler handler=JenaHandler.getInstance();
+		/*
+		 * This is a test class for run the program
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+		
+		
+		
+	
+			
+		try {
+			//handler.createDataTypeProperty("hasScore", "Player", "Player scores runs",XSD.xstring);
+		//	handler.addIndividuals("Batsman","Dilshan");
+		//	handler.addIndividuals("Team","Sri Lanka");
+			//	handler.addIndividuals("Team","India");
+			//handler.addObjectTypeProperty("India","playedAgainest","Sri Lanka");
+			handler.addDataTypeProperty("Sangakkara", "hasAverage", "54.5");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		
+		
+	}
+}
